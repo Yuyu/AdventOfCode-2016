91ebaffc1dd60b057de342e1f9d3122cb8dcf47d
Yuyu <admin@yuyu.io>
2016-12-18 13:46:57 +0100

    Day 18: Simplified the generation rule

--- day18/18.py
+++ day18/18.py
@@ -1,15 +1,14 @@
 import numpy as np
 
-# I've added one extra add both sides to include the "imaginary safe tiles"
 inp = ".^..^....^....^^.^^.^.^^.^.....^.^..^...^^^^^^.^^^^.^.^^^^^^^.^^^^^..^.^^^.^^..^.^^.^....^.^...^^.^."
 rows = 40 # 40 for Part 1, 400000 for Part 2
 
 tiles = np.ndarray((102, rows), dtype=bool)
+# I've added one extra add both sides to include the "imaginary safe tiles"
 tiles[:, 0] = [True] + map(lambda x: x == ".", list(inp)) + [True]
 
 for y in xrange(1, rows):
-    tiles[:, y] = [True] + map(lambda x: not ((not tiles[x-1, y-1] and tiles[x+1, y-1]) \
-        or (tiles[x-1, y-1] and not tiles[x+1, y-1])), xrange(1, 101)) + [True]
+    tiles[:, y] = [True] + map(lambda x: tiles[x-1, y-1] == tiles[x+1, y-1], xrange(1, 101)) + [True]
 
 # Subtract the imaginary safe tiles from the total sum of safe places
 print np.sum(tiles)-rows*2
